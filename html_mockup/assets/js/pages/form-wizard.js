dda0465d6be31d09da2d55a51ad066f8b5a3b748
Abraham Reyes <abe@areffect.co>
2015-07-29 15:39:18 -0400

    - Add provider wizard

--- html_mockup/assets/js/pages/form-wizard.js
+++ html_mockup/assets/js/pages/form-wizard.js
@@ -1,54 +1,4 @@
 $(document).ready(function() {
-    var $validator = $("#wizardForm").validate({
-        rules: {
-            exampleInputName: {
-                required: true
-            },
-            exampleInputName2: {
-                required: true
-		    },
-		    exampleInputEmail: {
-                required: true,
-                email: true
-		    },
-		    exampleInputPassword1: {
-                required: true
-		    },
-		    exampleInputPassword2: {
-                required: true,
-                equalTo: '#exampleInputPassword1'
-		    },
-		    exampleInputProductName: {
-                required: true
-		    },
-		    exampleInputProductId: {
-                required: true
-		    },
-		    exampleInputQuantity: {
-                required: true
-            },
-		    exampleInputCard: {
-                required: true,
-                number: true
-		    },
-		    exampleInputSecurity: {
-                required: true,
-                number: true
-		    },
-		    exampleInputHolder: {
-                required: true
-            },
-		    exampleInputExpiration: {
-                required: true,
-                date: true
-            },
-		    exampleInputCsv: {
-                required: true,
-                number: true
-            }
-        }
-    });
- 
     $('#rootwizard').bootstrapWizard({
         'tabClass': 'nav nav-tabs',
         onTabShow: function(tab, navigation, index) {
@@ -71,10 +21,5 @@ $(document).ready(function() {
                 return false;
             }
         },
-    });
-    
-    $('.date-picker').datepicker({
-        orientation: "top auto",
-        autoclose: true
     });
 });
