57e07421c188ae8cf6d0dd0f901b9ed1e6850715
Abe <ajrrac@gmail.com>
2015-08-17 12:33:31 -0400

    -- Added latest mockup to the home template -- Added latest CSS styles -- Commented LoginProviders.js and Load_Claims_v1.js

--- app/client/templates/home/home.js
+++ app/client/templates/home/home.js
@@ -72,24 +72,83 @@ Template['home'].rendered = function(){
         barColors: ['#6ad6c3','#22BAA0'],
         resize: true
     });
-*/
 
-// Count claims by type
+
+/* Count claims by type */
 
     var dentalCount = Claims.find({type : 'Dental'}).count();
     var pharmacyCount = Claims.find({type : 'Pharmacy'}).count();
     var medicalCount = Claims.find({type : 'Medical'}).count();
+    var visionCount = Claims.find({type : 'Vision'}).count();
+    var total = dentalCount+pharmacyCount+medicalCount+visionCount;
 
     Morris.Donut({
         element: 'morris4',
         data: [
+            {label: 'All', value: total },
             {label: 'Medical', value: medicalCount },
+            {label: 'Pharmacy', value: pharmacyCount },
             {label: 'Dental', value: dentalCount },
-            {label: 'Pharmacy', value: pharmacyCount }
+            {label: 'Vision', value: visionCount }
         ],
         resize: true,
-        colors: ['#74e4d1', '#44cbb4', '#119d85','#22BAA0'],
+        colors: ['rgb(34, 130, 186)', 'rgb(44, 154, 218)', 'rgb(89, 169, 216)','rgb(113, 180, 220)','rgb(146, 194, 222)'],
     });
+
+        Morris.Bar({
+        element: 'morris2',
+        data: [
+            { member: 'All', a: 370.13, b: 11500.00 },
+            { member: 'You', a: 0, b: 5750.00 },
+            { member: 'Abraham', a: 126.6, b: 5750.00  },
+            { member: 'Annabel', a: 43.53, b: 5750.00  },
+            { member: 'Thomas', a: 200, b: 5750.00  }
+        ],
+        xkey: 'member',
+        ykeys: ['a', 'b'],
+        labels: ['Contribution', 'Deductible'],
+        barRatio: 0.4,
+        xLabelAngle: 35,
+        hideHover: 'auto',
+        stacked: true,
+        barColors: ['rgba(34, 152, 186, 0.69)','rgb(34, 130, 186)'],
+        resize: true
+    });
+
+    Morris.Line({
+        element: 'morris3',
+        data: [
+            {month: '2015-01', a: 40, b: 785},
+            {month: '2015-02', a: 150, b: 1570},
+            {month: '2015-03', a: 190, b: 2355},
+            {month: '2015-04', a: 290, b: 3140},
+            {month: '2015-05', a: 330, b: 3925},
+            {month: '2015-06', a: 380, b: 4710},
+            {month: '2015-07', a: 420, b: 5495}
+        ],
+        xkey: 'month',
+        ykeys: ['a', 'b'],
+        labels: ['Member Savings','Insurance Cost'],
+        xLabels: 'month',
+        hideHover: 'auto',
+        lineColors: ['rgb(34, 130, 186)', 'rgb(89, 169, 216)'],
+        //goals: [8000],
+        resize: true,
+    });
+    /*
+    Morris.Donut({
+        element: 'morris4',
+        data: [
+            {label: 'All Claims', value: 370.13},
+            {label: 'Medical', value: 243.53},
+            {label: 'Pharmacy', value: 126.6 },
+            {label: 'Dental', value: 0 },
+            {label: 'Vision', value: 0 }
+        ],
+        resize: true,
+        colors: ['rgb(34, 130, 186)', 'rgb(44, 154, 218)', 'rgb(89, 169, 216)','rgb(113, 180, 220)','rgb(146, 194, 222)'],
+        formatter: function(y,data){return '$' + y}
+    });*/
 
     console.log(dentalCount, pharmacyCount, medicalCount);
 
--- html_mockup/assets/js/pages/charts-morris.js
+++ html_mockup/assets/js/pages/charts-morris.js
@@ -2,10 +2,11 @@ $( document ).ready(function() {
     Morris.Bar({
         element: 'morris2',
         data: [
+            { member: 'All', a: 370.13, b: 11500.00 },
             { member: 'You', a: 0, b: 5750.00 },
-            { member: 'Abraham', a: 0, b: 5750.00  },
-            { member: 'Annabel', a: 198.85, b: 5750.00  },
-            { member: 'Thomas', a: 100, b: 5750.00  }
+            { member: 'Abraham', a: 126.6, b: 5750.00  },
+            { member: 'Annabel', a: 43.53, b: 5750.00  },
+            { member: 'Thomas', a: 200, b: 5750.00  }
         ],
         xkey: 'member',
         ykeys: ['a', 'b'],
@@ -14,20 +15,42 @@ $( document ).ready(function() {
         xLabelAngle: 35,
         hideHover: 'auto',
         stacked: true,
-        barColors: ['#6ad6c3','#22BAA0'],
+        barColors: ['rgba(34, 152, 186, 0.69)','rgb(34, 130, 186)'],
         resize: true
+    });
+
+    Morris.Line({
+        element: 'morris3',
+        data: [
+            {month: '2015-01', a: 40, b: 785},
+            {month: '2015-02', a: 150, b: 1570},
+            {month: '2015-03', a: 190, b: 2355},
+            {month: '2015-04', a: 290, b: 3140},
+            {month: '2015-05', a: 330, b: 3925},
+            {month: '2015-06', a: 380, b: 4710},
+            {month: '2015-07', a: 420, b: 5495}
+        ],
+        xkey: 'month',
+        ykeys: ['a', 'b'],
+        labels: ['Member Savings','Insurance Cost'],
+        xLabels: 'month',
+        hideHover: 'auto',
+        lineColors: ['rgb(34, 130, 186)', 'rgb(89, 169, 216)'],
+        //goals: [8000],
+        resize: true,
     });
 
     Morris.Donut({
         element: 'morris4',
         data: [
+            {label: 'All Claims', value: 370.13},
             {label: 'Medical', value: 243.53},
             {label: 'Pharmacy', value: 126.6 },
             {label: 'Dental', value: 0 },
             {label: 'Vision', value: 0 }
         ],
         resize: true,
-        colors: ['#74e4d1', '#44cbb4', '#119d85','#22BAA0'],
+        colors: ['rgb(34, 130, 186)', 'rgb(44, 154, 218)', 'rgb(89, 169, 216)','rgb(113, 180, 220)','rgb(146, 194, 222)'],
         formatter: function(y,data){return '$' + y}
     });
 });
