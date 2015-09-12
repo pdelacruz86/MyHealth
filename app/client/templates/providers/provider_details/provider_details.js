d73c529e6f3100590d16bd8631dcb53f75942fa6
Abraham Reyes <abe@areffect.co>
2015-09-11 21:05:58 -0400

    - style.css changes around pricing-tables. - ui changes 	- templates 		- home 		- header 		- provider - profile template creation - Implementation of both spinner and loader gifs. - and more...

--- app/client/templates/providers/provider_details/provider_details.js
+++ app/client/templates/providers/provider_details/provider_details.js
@@ -2,6 +2,25 @@
 /* ProviderDetails: Event Handlers */
 /*****************************************************************************/
 Template.ProviderDetails.events({
+	'click #reload-button': function(){
+		console.log('Entro');
+		event.preventDefault();
+		Meteor.call("provider/reload_user_data","aetna",function(error,result){
+			if(error){
+				console.log('Eploto el metodo reload_user_data');
+			} else {
+				Router.go("/");
+
+				Meteor.call("load_claims_rates_no_options",function(error,result){
+					if(error){
+						console.log('Eploto la carga de el detalle');
+					} else {
+						console.log('Carga de el detalle iniciada');
+					}
+				});
+			}
+		});
+	}
 });
 
 /*****************************************************************************/
